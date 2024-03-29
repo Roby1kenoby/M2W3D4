21529517cf8b71d54ba00aec741acc5dee286f75
Roberto Allocco <r.allocco@pragmos.it>
2024-03-29 21:06:42 +0100

    solved all the requests

--- s3p2-scaricabile.js
+++ s3p2-scaricabile.js
@@ -16,9 +16,80 @@
 //ESERCIZI IN JS
 // ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
 // ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
-// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
+// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel footer in un altro, fittizio
 // ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
 // ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
 // ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
 
+// standard background color
+let stdBgColor = "white"
+// toggle variable to revert link class with a second click on the button
+let toggleLinkColor = false
+// toggle to revert image visibility
+let toggleImgVisibility = false
 
+function changeH1Text(){
+    let actualH1 = document.getElementsByTagName("h1")[0]
+    const oldH1 = 'Il Paradiso Della Brugola'
+    const newH1 = 'La Brugola in Paradiso'
+    actualH1.textContent = actualH1.textContent === oldH1 ? newH1 : oldH1 
+}
+
+function changeBgColor(){
+    let bd = document.getElementsByTagName("body")[0]
+    // check also if the background color is set, otherwise the first click will not work
+    bd.style.backgroundColor = bd.style.backgroundColor === stdBgColor || !bd.style.backgroundColor ? "pink" : stdBgColor
+}
+
+function changeFooterAddress(){
+    let actualFooter = document.getElementsByTagName("footer")[0]
+    const oldFooter = 'Il Paradiso Della Brugola s.r.l. - Via di qui 45, Torino (TO), 10123, Italia'
+    const newFooter = 'La Brugola in Paradiso s.r.l. - Piazza la Bomba e Scappa 93, Milano (MI), 12345, Italia'
+    actualFooter.textContent = actualFooter.textContent === oldFooter ? newFooter : oldFooter 
+}
+
+function addClassToLink(){
+    let links = document.getElementsByTagName("a")    
+    let extraClass = 'linkExtraClass'
+    if (!toggleLinkColor){
+        toggleLinkColor = !toggleLinkColor
+        for (let l of links){
+            l.className = extraClass
+        }
+    }
+    else{
+        toggleLinkColor = !toggleLinkColor
+        for (let l of links){
+            l.className = ''
+        }
+    }
+}
+
+function toggleImgs(){
+    let imgs = document.getElementsByTagName("img")    
+    let extraClass = 'imgExtraClass'
+    if (!toggleImgVisibility){
+        toggleImgVisibility = !toggleImgVisibility
+        for (let i of imgs){
+            i.className = extraClass
+        }
+    }
+    else{
+        toggleImgVisibility = !toggleImgVisibility
+        for (let i of imgs){
+            i.className = ''
+        }
+    }
+}
+
+function changePricesColor(){
+    let prices = document.getElementsByClassName("price")
+    
+    for (let p of prices){
+        // grabbed this from the internet, i suppose it randomize a number between 0 and 1, and the multiplication convert it to a valid number 
+        // that the toString function convert to be postponed to the #
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);    
+        p.style.color = "#" + randomColor
+        console.log(p.style.color)
+    }
+}
